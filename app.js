5687d328ccfa83061e40d5464db5ad6619f2c9f9
italloalexsander <italloalexsander@gmail.com>
2022-11-18 11:26:02 -0300

    Add content to about and contact pages

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.get("/", (req, res) => {
   res.render("home", {homeStartingContent: homeStartingContent});
 })
 
+app.get("/about", (req, res) => {
+  res.render("about", {aboutContent: aboutContent});
+})
+
+app.get("/contact", (req, res) =>{
+  res.render("contact", {contactContent: contactContent});
+})
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 })
