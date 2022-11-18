28a4d85be033130c24542bce3f48eb526c140bb9
italloalexsander <italloalexsander@gmail.com>
2022-11-18 11:06:08 -0300

    First Setup

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", (req, res)=>{
+  res.send("Hello");
+})
+
+app.listen(3500, ()=>{
+  console.log("Server started on port 3500")
+})
 
 
 
